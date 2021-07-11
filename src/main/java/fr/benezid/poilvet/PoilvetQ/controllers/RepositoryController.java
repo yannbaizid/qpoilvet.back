7d6a249821aeec04de18051330d3f5dbd45a27ff
ybaizid <ybaizid@hsoftware.com>
2021-07-11 22:29:59 +0200

    this work

--- src/main/java/fr/benezid/poilvet/PoilvetQ/controllers/RepositoryController.java
+++ src/main/java/fr/benezid/poilvet/PoilvetQ/controllers/RepositoryController.java
@@ -2,11 +2,7 @@ package fr.benezid.poilvet.PoilvetQ.controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
@@ -16,11 +12,10 @@ public interface RepositoryController<T> {
 
 	@GetMapping("/getall")
 	@CrossOrigin
-	public  List<T> getAll();
-	
-	
-	
-	@PostMapping("/save")
+	List<T> getAll();
+
+
+	@PostMapping(path = "/save")
 	@CrossOrigin
-	public T save(@RequestBody T item);
+	T save(@RequestBody T object);
 }
